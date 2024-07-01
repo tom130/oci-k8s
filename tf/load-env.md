bd76b74a63cc4f3d93803d40440e0cdd011d8a75
Tomas Krbec <mail@tomaskrbec.cz>
2024-07-01 14:53:29 +0200

    feat: initial config for cluster
    Signed-off-by: Tomas Krbec <mail@tomaskrbec.cz>

---
+++ tf/load-env.md
@@ -0,0 +1,5 @@
+# ENV VARS
+env vars are stored in bitwarden to load them use 
+```
+source <(rbw get tf-oci-k8s)
+```
---
+++ tf/variables-private.tf
@@ -0,0 +1,24 @@
+variable "compartment_id" {
+  type        = string
+  description = "The compartment to create the resources in"
+}
+variable "region" {
+  type        = string
+  description = "The region to provision the resources in"
+  default     = "eu-frankfurt-1"
+}
+variable "ssh_public_key" {
+  type        = string
+  description = "The SSH public key to use for connecting to the worker nodes"
+  default     = "ssh-rsa AAAA........"
+}
+variable "bastion_allowed_ips" {
+  type        = list(string)
+  description = "List of IP prefixes allowed to connect via bastion"
+  default     = ["127.0.0.1/32"]
+}
+variable "ad_list" {
+  type        = list
+  description = "List of length 2 with the names of availability regions to use"
+  default     = ["ZIjq:EU-FRANKFURT-1-AD-1", "ZIjq:EU-FRANKFURT-1-AD-2"]
+}
